c766460ef02bc11bcbb9eb3d085d7fb57db2522b
hacker5657 <88392095+hacker5657@users.noreply.github.com>
2024-06-02 20:38:18 +0300

    Update HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -56,7 +56,7 @@ export default {
       this.json = JSON.stringify(JSON.parse(this.json), null, 4)
     },
     setResultUrl() {
-      let newUrl = `${process.env.SITE_URL}/result?`
+      let newUrl = 'https://twig-sandbox-v2-backend.vercel.app/result?'
 
       this.$store.commit('setJsonValue', this.json)
       this.$store.commit('setTemplateValue', this.template)
