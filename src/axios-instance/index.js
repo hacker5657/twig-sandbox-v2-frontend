aa8f1d9a165e3d6dbd41e8b4123a8f9595c51d17
hacker5657 <88392095+hacker5657@users.noreply.github.com>
2024-06-11 16:31:20 +0300

    Update index.js

--- src/axios-instance/index.js
+++ src/axios-instance/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://twig-sandbox-v2-.onrender.com/api',
+  baseURL: 'https://twig-sandbox-v2.onrender.com/api',
   withCredentials: true
 })
